9b78f6b760e183de097c98faa65a1b5f15c67ad2
Roshan Kumar <roshankumarxc@gmail.com>
2025-04-30 22:07:59 +0530

    added

---
+++ smallest/largest.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int nums;
+    int nums[] = {5,15,22,1,-15,24};
+    int size = 6;
+
+    int smallest = INT16_MAX;
+
+    for(int i = 0; i<size; i++){
+
+    }
+
+    return 0;
+
+}
